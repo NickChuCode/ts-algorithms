0875024ccc5b59600a6988be05005a58928315e3
NickChuCode <nickchu15@outlook.com>
2019-06-23 19:17:16 +0800

    feat(types): 添加 Queue 的接口定义

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,11 @@ export interface Stack<T> {
   clear(): void
   size(): number
 }
+
+export interface Queue<T> {
+  enqueue(element: T): void
+  dequeue(): T | undefined
+  peek(): T | undefined
+  isEmpty(): boolean
+  size(): number
+}
